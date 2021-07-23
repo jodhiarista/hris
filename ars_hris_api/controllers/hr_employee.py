0b1ac35e455a5ee9344ba6b60bbbb6af5a9fe44b
Jodhi Satrio <jodhi.satrio@arista-group.co.id>
2021-07-23 10:02:02 +0700

    Added dictionary

--- ars_hris_api/controllers/hr_employee.py
+++ ars_hris_api/controllers/hr_employee.py
@@ -9,6 +9,6 @@ class HrEmployee(http.Controller):
 
     @http.route('/hr_employee/update_from_hris', auth='public', type='json', csrf=False)
     def update_employee_from_hris(self, datas):
-        employee = request.env['hr.employee'].search([('nik_temp', '=', datas.nik_temp)], limit=1)
+        employee = request.env['hr.employee'].search([('nik_temp', '=', datas['nik_temp'])], limit=1)
         employee.write(datas)
         return ("Success edited employee with ID " + str(employee.id))
