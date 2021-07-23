baae25237de3c9bf4e3314b45ef419081cb133ff
Jodhi Satrio <jodhi.satrio@arista-group.co.id>
2021-07-23 09:55:11 +0700

    Added @

--- ars_hris_api/controllers/hr_employee.py
+++ ars_hris_api/controllers/hr_employee.py
@@ -7,7 +7,7 @@ class HrEmployee(http.Controller):
         created_employee = request.env['hr.employee'].sudo().create(datas)
         return ("Success create employee with ID " + str(created_employee.id))
 
-    http.route('/hr_employee/update_from_hris', auth='public', type='json', csrf=False)
+    @http.route('/hr_employee/update_from_hris', auth='public', type='json', csrf=False)
     def update_employee_from_hris(self, datas):
         employee = request.env['hr.employee'].search([('nik_temp', '=', datas.nik_temp)], limit=1)
         employee.write(datas)
