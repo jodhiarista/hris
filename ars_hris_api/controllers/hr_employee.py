24406fa08206c65149d999dbce65fcbf015dea12
Jodhi Satrio <jodhi.satrio@arista-group.co.id>
2021-07-23 09:47:00 +0700

    changed treturn to str

--- ars_hris_api/controllers/hr_employee.py
+++ ars_hris_api/controllers/hr_employee.py
@@ -5,10 +5,10 @@ class HrEmployee(http.Controller):
     @http.route('/hr_employee/create_from_hris', auth='public', type='json', csrf=False)
     def create_employee_from_hris(self, datas):
         created_employee = request.env['hr.employee'].sudo().create(datas)
-        return ("Success create employee with ID " + created_employee.id)
+        return ("Success create employee with ID " + str(created_employee.id))
 
     http.route('/hr_employee/update_from_hris', auth='public', type='json', csrf=False)
     def update_employee_from_hris(self, datas):
         employee = request.env['hr.employee'].search([('nik_temp', '=', datas.nik_temp)], limit=1)
         employee.write(datas)
-        return ("Success edited employee with ID " + employee.id)
+        return ("Success edited employee with ID " + str(employee.id))
