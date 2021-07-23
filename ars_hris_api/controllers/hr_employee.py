c20432c5773490a20b1f4256925089c19f70ed9a
Jodhi Satrio <jodhi.satrio@arista-group.co.id>
2021-07-23 09:37:46 +0700

    changed to datas

--- ars_hris_api/controllers/hr_employee.py
+++ ars_hris_api/controllers/hr_employee.py
@@ -4,17 +4,7 @@ from odoo.http import request
 class HrEmployee(http.Controller):
     @http.route('/hr_employee/create_from_hris', auth='public', type='json', csrf=False)
     def create_employee_from_hris(self, datas):
-        created_employee = request.env['hr.employee'].sudo().create({
-            'name': datas.name,
-            'nik_temp': datas.nik_temp,
-            'job_title': datas.job_title,
-            'place_of_birth': datas.place_of_birth,
-            'birthday': datas.birthday,
-            'private_email': datas.private_email,
-            'work_email': datas.work_email,
-            'identification_id': datas.identification_id,
-            'work_location': datas.work_location
-        })
+        created_employee = request.env['hr.employee'].sudo().create(datas)
         return ("Success create employee with ID " + created_employee.id)
 
     http.route('/hr_employee/update_from_hris', auth='public', type='json', csrf=False)
